02c20d8fa62da6d1fde2b7ede8bc81aebdb745af
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-08-04 22:18:35 +0530

    User Authentication Completed.

--- src/djcrm/urls.py
+++ src/djcrm/urls.py
@@ -17,12 +17,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
-from leads.views import LandingPageView
+from django.contrib.auth.views import LoginView, LogoutView
+from leads.views import LandingPageView, SignupView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls', namespace="leads")),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout")
 ]
 
 if settings.DEBUG:
--- src/leads/forms.py
+++ src/leads/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 from .models import Lead
+
+User = get_user_model()
+
 
 class LeadModelForm(forms.ModelForm):
     class Meta:
@@ -15,3 +20,9 @@ class LeadForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
     age = forms.IntegerField(min_value=0)
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        field_classes = {'username': UsernameField}
