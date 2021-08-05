df7e59f4a9a1c1d464fc30173f347e27298689d2
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-08-05 13:15:55 +0530

    Fix few changes

--- src/agents/views.py
+++ src/agents/views.py
@@ -6,7 +6,9 @@ from .forms import AgentModelForm
 
 class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
-    queryset = Agent.objects.all()
+    
+    def get_queryset(self):
+        return Agent.objects.all()
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = "agents/agent_create.html"
@@ -23,20 +25,26 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
 
 class AgentDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "agents/agent_detail.html"
-    queryset = Agent.objects.all()
     context_object_name = "agent"
+
+    def get_queryset(self):
+        return Agent.objects.all()
 
 class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
     template_name = "agents/agent_update.html"
-    queryset = Agent.objects.all()
     form_class = AgentModelForm
+
+    def get_queryset(self):
+        return Agent.objects.all()
 
     def get_success_url(self):
         return reverse("agents:agent-list")
 
 class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
     template_name = "agents/agent_delete.html"
-    queryset = Agent.objects.all()
+
+    def get_queryset(self):
+        return Agent.objects.all()
 
     def get_success_url(self):
         return reverse("agents:agent-list")
