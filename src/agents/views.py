bd953f00c9940169532a4b58536f9bcf29a34c75
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-11-02 15:41:45 +0530

    Fix few mistakes

--- src/agents/views.py
+++ src/agents/views.py
@@ -10,10 +10,11 @@ from .mixins import OrganizerAndLoginRequiredMixin
 
 class AgentListView(OrganizerAndLoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
-    
+
     def get_queryset(self):
         organization = self.request.user.userprofile
         return Agent.objects.filter(organization=organization)
+
 
 class AgentCreateView(OrganizerAndLoginRequiredMixin, generic.CreateView):
     template_name = "agents/agent_create.html"
@@ -24,7 +25,7 @@ class AgentCreateView(OrganizerAndLoginRequiredMixin, generic.CreateView):
 
     def form_valid(self, form):
         user = form.save(commit=False)
-        user.is_agent =  True
+        user.is_agent = True
         user.is_organizer = False
         user.set_password(f"{random.randint(0, 100000)}")
         user.save()
@@ -40,6 +41,7 @@ class AgentCreateView(OrganizerAndLoginRequiredMixin, generic.CreateView):
         )
         return super(AgentCreateView, self).form_valid(form)
 
+
 class AgentDetailView(OrganizerAndLoginRequiredMixin, generic.DetailView):
     template_name = "agents/agent_detail.html"
     context_object_name = "agent"
@@ -48,16 +50,18 @@ class AgentDetailView(OrganizerAndLoginRequiredMixin, generic.DetailView):
         organization = self.request.user.userprofile
         return Agent.objects.filter(organization=organization)
 
+
 class AgentUpdateView(OrganizerAndLoginRequiredMixin, generic.UpdateView):
     template_name = "agents/agent_update.html"
     form_class = AgentModelForm
 
-    def get_queryset(self):
-        return Agent.objects.all()
-
     def get_success_url(self):
+        return reverse("agents:agent-list")
+
+    def get_queryset(self):
         organization = self.request.user.userprofile
         return Agent.objects.filter(organization=organization)
+
 
 class AgentDeleteView(OrganizerAndLoginRequiredMixin, generic.DeleteView):
     template_name = "agents/agent_delete.html"
